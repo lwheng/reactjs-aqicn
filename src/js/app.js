de4f398e9a7ead2de940647330c9c9b0154f3bc0
Low-Wee Heng <alpha116@gmail.com>
2021-04-09 22:32:42 +0800

    Able to call api to fetch json content

--- src/js/app.js
+++ src/js/app.js
@@ -2,24 +2,34 @@
 
 const e = React.createElement;
 
-class LikeButton extends React.Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { liked: false };
+    this.state = { content: "Nothing to display" };
+  }
+
+  componentDidMount() {
+    fetch("https://api.waqi.info/feed/geo:1.367261;103.79982/?token=49441845ec0f10680db9bafd791d935630d6bb28")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            content: JSON.stringify(result)
+          });
+        },
+
+        (error) => {
+          this.setState({
+            content: "Error!"
+          });
+        }
+      )
   }
 
   render() {
-    if (this.state.liked) {
-      return 'You liked this.';
-    }
-
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
-    );
+    return this.state.content;
   }
 }
 
 const domContainer = document.querySelector('#root');
-ReactDOM.render(e(LikeButton), domContainer);
+ReactDOM.render(e(App), domContainer);
